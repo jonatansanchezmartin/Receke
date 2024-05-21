7d11863efeee35ca086881c719854e170cc9fc92
sanzdecastro <sanzdecastro@gmail.com>
2024-05-21 14:06:40 +0200

    test

--- receke/src/views/AddRecipe.vue
+++ receke/src/views/AddRecipe.vue
@@ -25,7 +25,7 @@ export default {
     <div class="query-nav">
         <button><router-link to="/"><img src="./src/assets/img/back-arrow.png"></router-link>  </button>
         <div>
-            <h1>Contribuye con tu receta</h1>
+            <h1>Contribuye con tu receta TEST</h1>
             <p>Aporta a la comunidad recetas fáciles con ingredientes sencillos</p>
         </div>
     </div>
