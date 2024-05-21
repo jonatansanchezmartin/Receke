4edc263d2aacb83e2ecfdc58341401af5d407368
sanzdecastro <sanzdecastro@gmail.com>
2024-05-21 11:28:57 +0200

    add route

--- receke/src/router/index.js
+++ receke/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import AddRecipe from '../views/AddRecipe.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/add-recipe',
+      name: 'add-recipe',
+      component: AddRecipe
     }
     // {
     //   path: '/add-recipe',
--- receke/src/views/AddRecipe.vue
+++ receke/src/views/AddRecipe.vue
@@ -2,7 +2,13 @@
 </script>
 
 <template>
-
+    <div class="query-nav">
+        <button><router-link to="/home"><img src="./src/assets/img/back-arrow.png"></router-link>  </button>
+        <div>
+            <h1>Contribuye con tu receta</h1>
+            <p>Aporta a la comunidad recetas fáciles con ingredientes sencillos</p>
+        </div>
+    </div>  
 </template>
 
 <style>
