9153d893acd9af33b4436e15b747b4bdf7494387
Jordi <jordijs6@gmail.com>
2024-05-23 22:20:51 +0200

    feat: add functionality to select or deselect ingredients, adding them to an array of selected ingredients on the ingredients store

--- receke/src/stores/IngredientsStore.js
+++ receke/src/stores/IngredientsStore.js
@@ -4,20 +4,12 @@ import { defineStore } from 'pinia'
 export const useIngredientsStore = defineStore('IngredientsStore', {
   state: () => ({
     ingredients: [],
+    selectedIngredients: [],
     categories: []
     //ingredients : parseInt(localStorage.getItem('ingredients') || [])
   }),
   getters: {},
   actions: {
-    // increment() {
-    //   this.count++
-    //   localStorage.setItem('count', this.count)
-    // },
-    // decrement() {
-    //   this.count--
-    //   localStorage.setItem('count', this.count)
-    // }
-
     async fetchIngredients() {
       let response = await fetch('http://localhost:3001/ingredients')
       let ingredients = await response.json()
@@ -28,14 +20,15 @@ export const useIngredientsStore = defineStore('IngredientsStore', {
       let response = await fetch('http://localhost:3001/categories')
       let categories = await response.json()
       this.categories = categories
-    }
+    },
+
+    toggleIngredient(id) {
+      if (this.selectedIngredients.includes(id)) {
+          this.selectedIngredients = this.selectedIngredients.filter(ingredient => ingredient !== id)
+      } else {
+          this.selectedIngredients.push(id)
+      } 
+  }
   }
 
-  // const count = ref(0)
-  // const doubleCount = computed(() => count.value * 2)
-  // function increment() {
-  //   count.value++
-  // }
-
-  // return { count, doubleCount, increment }
 })
