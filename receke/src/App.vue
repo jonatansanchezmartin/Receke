476534fa6cf2703dd3fecba9adcfa10e8902c136
Jordi <jordijs6@gmail.com>
2024-05-23 21:28:53 +0200

    refactor: cleanup and formatting

--- receke/src/App.vue
+++ receke/src/App.vue
@@ -12,29 +12,12 @@ export default {
     computed: {
         ...mapState(useIngredientsStore, ['ingredients','categories'])
     },
-    data() {
-        return {
-            //ingredients: [],
-            //categories: []
-        }
-    },
     created() {
         this.fetchIngredients()
         this.fetchCategories()
     },
     methods: {
         ...mapActions(useIngredientsStore, ['fetchIngredients','fetchCategories']),
-        // async fetchIngredients() {
-        //     let response = await fetch('http://localhost:3001/ingredients')
-        //     let ingredients = await response.json()
-        //     this.ingredients = ingredients
-        // },
-
-        // async fetchCategories() {
-        //     let response = await fetch('http://localhost:3001/categories')
-        //     let categories = await response.json()
-        //     this.categories = categories
-        // }
     }
 }
 </script>
