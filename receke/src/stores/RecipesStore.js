58a69bcfb3148628022cb6c7701c1b278ec21f0f
sanzdecastro <sanzdecastro@gmail.com>
2024-05-24 15:26:03 +0200

    format recipeStore

--- receke/src/stores/RecipesStore.js
+++ receke/src/stores/RecipesStore.js
@@ -1,49 +1,27 @@
 //import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useRecipesStore = defineStore('RecipesStore', {  //el default
-  state: () => ({   //data de default
-    recipes : [],
-
-    // recipeName: '',
-
-    // selectedIngredients: [],
-           
-
-    // steps: ["pasoDummy"],
-
-    // recipeImage: '',
-
-    // isActive: false,
-
-    //ingredients : parseInt(localStorage.getItem('ingredients') || []), //obtiene los ingredientes guardados en LocalStorage en futuro quitamos esto cuando pongamos usuarios
-  
-     
+export const useRecipesStore = defineStore('RecipesStore', {
+  //el default
+  state: () => ({
+    //data de default
+    recipes: []
   }),
-  getters: {
+  getters: {},
+  actions: {
+    //methods de default
 
-  },
-  actions: {    //methods de default
-    
-
-    
-    async postRecipe(newRecipe){
-      const url = "http://localhost:3001/recipes";
+    async postRecipe(newRecipe) {
+      const url = 'http://localhost:3001/recipes'
       await fetch(url, {
-          method: 'POST',
-          headers: {
+        method: 'POST',
+        headers: {
           'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(newRecipe)
+        },
+        body: JSON.stringify(newRecipe)
       })
-      
-      window.location.reload();
+
+      window.location.reload()
+    }
   }
-}
-
-    
-
-  }
-
-
-)
+})
