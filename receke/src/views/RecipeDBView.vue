fc97827b62c326d0a5187dac5bf3b32e13cba1a9
Ana Fdez <afminguela@gmail.com>
2024-05-24 15:21:40 +0200

    saved at 15 pm

--- receke/src/views/RecipeDBView.vue
+++ receke/src/views/RecipeDBView.vue
@@ -1,19 +1,9 @@
 <script>
 </script>
 <template>
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-    <meta charset="UTF-8">
-    <link rel="icon" href="/favicon.ico">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Receké</title>
-    <link rel="stylesheet" type="text/css" href="./src/assets/main.css">
-    </head>
+    
 
-
-    <body>
-    <div id="app">
+    
 
         <!-- Header logo, content & navigation -->
 
@@ -83,10 +73,7 @@
 
 
 
-    </div>
-    <script type="module" src="/src/main.js"></script>
-    </body>
-    </html>
-
+  
+    
 </template>
 <style></style>
