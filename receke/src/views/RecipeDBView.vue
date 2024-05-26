1b69b1e25513f5d1adf281bed37984a84a017052
Jordi <jordijs6@gmail.com>
2024-05-26 23:38:05 +0200

    fix: display images of the ingredients, and get elements from store using mapState/Actions

--- receke/src/views/RecipeDBView.vue
+++ receke/src/views/RecipeDBView.vue
@@ -1,89 +1,61 @@
 <script>
-    import { mapState } from "pinia";
-    import { useIngredientsStore } from '@/stores/IngredientsStore';
-    // import { useRecipesStore } from '@/stores/RecipesStore';
-    export default{
-        // data(){
-        //     return{
-        //         // Aquí dentro meto todas las variables que solo utilice en las funciones de aquí
-        //         recipesStore : useRecipesStore(),
-        //     }
-        // },
-        // methods: {
-        //     // Aquí dentro meto todas las funciones que cree para usar aquí
-        //     getRecipesById(){
-        //         this.recipesStore.getRecipeById(this.$route.params.id)
-        //     },
-
-        //     // getIngredientsById(){
-        //     //     console.log(ingredients)
-        //     // }
-        // },
-
-        computed: {
-        ...mapState(useIngredientsStore, ['ingredients']),
-        // ...mapActions(useIngredientsStore, ['getIngredientById']),
-        },
-
-        // mounted() {
-        //     this.getRecipesById()
-        //     // this.getIngredientsById()
-        // }, 
+import { mapState, mapActions } from "pinia";
+import { useIngredientsStore } from '@/stores/IngredientsStore';
+import { useRecipesStore } from '@/stores/RecipesStore';
+export default {
+    methods: {
+        ...mapActions(useRecipesStore, ['getRecipeById']),
+        getImage(ingredient) {
+            console.log('received', ingredient)
+            const ingredientMatch = this.ingredients.find((item) => item.name === ingredient)
+            return ingredientMatch.image
+        }
+    },
+    computed: {
+        ...mapState(useRecipesStore, ['recipeSelected']),
+        ...mapState(useIngredientsStore, ['ingredients'])
+    },
+    mounted() {
+        this.getRecipeById(this.$route.params.id)
     }
+}
 </script>
 
 <template>
-        <button @click="console.log(ingredients)">Test Recipes</button>
 
-        <div v-for="ingredient of ingredients" :key="ingredient.id">
-            <img :src=ingredient.image />
+    <header class="header-logo">
+        <img src="./src/assets/img/Receké.png" alt="Logo">
+    </header>
+
+    <!-- Recipe title -->
+    <div class="recipe-nav">
+        <button> <img src="./src/assets/img/back-arrow.png"> </button>
+        <h1>{{ recipeSelected.title }}</h1>
+    </div>
+
+    <!-- Recipe image -->
+    <div class="recipe-image"> <img :src="recipeSelected.image" width="250"> </div>
+    <h3 class="ingredients-title"> Ingredientes</h3>
+
+    <!-- Ingredients list -->
+    <div class="ingredients-list" v-for="(item, index) in recipeSelected.ingredients" :key="index">
+        <div>
+            <img :src="getImage(item.ingredient)">
+            <h4> {{ item.ingredient }} {{ item.quantity }}</h4>
         </div>
-        <!-- Recipe title -->
+    </div>
 
+    <!-- Ingredients -->
+    <div class="steps-menu">
+        <h3 class="ingredients-title"> Pasos</h3>
+        <p>Ver pasos en pictogramas</p>
+    </div>
 
-        <!-- <div class="recipe-nav">
-            <button> <img src="./src/assets/img/back-arrow.png"> </button>
-            <h1>{{ recipesStore.recipeSelected.title }}</h1>
-        </div>   -->
-
-        <!-- Recipe image -->
-
-        <!-- <div class="recipe-image"> <img :src="recipesStore.recipeSelected.image" width="250"> </div>
-
-        <h3 class="ingredients-title"> Ingredientes</h3> -->
-
-        <!-- Ingredients list -->
-
-        <!-- <div class="ingredients-list" v-for="(item, index) in recipesStore.recipeSelected.ingredients" :key="index">
-
-            <div>  -->
-                <!-- <img src="./src/assets/media/tomate.png"> -->
-                <!-- <h4> {{ item.ingredient }} {{ item.quantity }}</h4> -->
-            <!-- </div>
-
-        </div> -->
-
-
-
-        <!-- Ingredients -->
-
-
-        <!-- <div class="steps-menu"> 
-            <h3 class="ingredients-title"> Pasos</h3>
-            <p>Ver pasos en pictogramas</p>
-        </div>
-
-
-        <div class="recipe-steps">
-            <ol>
-                <li v-for="step in recipesStore.recipeSelected.instructions"> {{step}}</li>
-            </ol>
-        </div> -->
-
-
-
-
-
+    <div class="recipe-steps">
+        <ol>
+            <li v-for="(step, index) in recipeSelected.instructions" :key="index"> {{ step }} </li>
+        </ol>
+    </div>
 
 </template>
 <style></style>
