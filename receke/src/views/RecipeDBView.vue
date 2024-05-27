ba8d8ccd7f03c934267e1f1e5928c2b5090ab586
sanzdecastro <sanzdecastro@gmail.com>
2024-05-27 16:14:53 +0200

    comment cosas locas

--- receke/src/views/RecipeDBView.vue
+++ receke/src/views/RecipeDBView.vue
@@ -21,7 +21,7 @@ export default {
 </script>
 
 <template>
-    <!DOCTYPE html>
+    <!-- <!DOCTYPE html>
     <html lang="en">
     <head>
     <meta charset="UTF-8">
@@ -69,7 +69,7 @@ export default {
         <ol>
             <li v-for="(step, index) in recipeSelected.instructions" :key="index"> {{ step }} </li>
         </ol>
-    </div>
+    </div> -->
 
 </template>
 <style></style>
