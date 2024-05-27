298b1b99590b654ed66de103152818d17d8383db
Ana Fdez <afminguela@gmail.com>
2024-05-27 20:28:19 +0200

    recipesView con arrow de vuelta, lunes 2028

--- receke/src/views/RecipeDBView.vue
+++ receke/src/views/RecipeDBView.vue
@@ -27,7 +27,7 @@ export default {
     
     <!-- Recipe title -->
     <div class="recipe-nav">
-        <button> <img src="./src/assets/img/back-arrow.png"> </button>
+        <button><router-link to="/"><img src="../assets/img/back-arrow.png"></router-link> </button>
         <h1>{{ recipeSelected.title }}</h1>
     </div>
 
