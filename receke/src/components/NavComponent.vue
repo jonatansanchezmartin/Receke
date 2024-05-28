63961cb716aeddbc3d0895ab10bda19b4de67b40
Jordi <jordijs6@gmail.com>
2024-05-28 11:56:08 +0200

    refactor: extract nav to NavComponent, passing props: title and link

---
+++ receke/src/components/NavComponent.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    name: 'NavComponent',
+    props: {
+        link: String,
+        title: String,
+    },
+}
+</script>
+
+<template>
+    <div class="recipe-nav">
+        <router-link :to="link">
+            <button><img src="../assets/img/back-arrow.png" /></button>
+        </router-link>
+        <h1>{{ title }}</h1>
+    </div>
+</template>
+
+<style></style>
