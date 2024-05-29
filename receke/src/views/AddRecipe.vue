5fc76ee15b706511f6caa4b504634509c365dcbe
sanzdecastro <sanzdecastro@gmail.com>
2024-05-29 11:28:57 +0200

    styles desktop form

--- receke/src/views/AddRecipe.vue
+++ receke/src/views/AddRecipe.vue
@@ -57,53 +57,69 @@ export default {
 
 <template>
   <NavComponent link="/" title="Contribuye con tu receta" />
-  <form  @submit.prevent="createRecipe(this.recipeName, this.steps, this.selectedIngredients, this.recipeImage)">
-    <div class="form-block form-recipe-name">
-      <label class="ingredients-title" for="recipe-name">Nombre de la receta</label>
-      <input id="recipe-name"  v-model="recipeName" />
-    </div>
-    <div class="form-block form-recipe-name">
-      <label class="ingredients-title" for="recipe-name">Ingredientes básicos</label>
-      <div class="cards-container">
-        <ingredientCard
-          v-for="ingredient in ingredients"
-          :selectedIngredients="selectedIngredients"
-          :ingredients="ingredients"
-          :ingredient="ingredient"
-          :key="ingredient.id"
-        />
-        <!-- <div class="ingredient-card"  > 
-                </div> -->
-      </div>
-    </div>
-    <div class="form-block recipe-steps-container form-recipe-name">
-      <label for="steps">Pasos</label>
-
-      <ol>
-        <newStep
-          v-for="step in steps"
-          :key="step"
-          :steps="steps"
-          @add-step="(descriptionStep) => addStep(descriptionStep)"
-        />
-      </ol>
-    </div>
-    <div class="form-block form-recipe-name">
-      <label class="ingredients-title" for="recipe-img">Imagen de la receta</label>
-      <input id="recipe-img" v-model="recipeImage" />
-    </div>
-
-    
-    <button :class="{ 'disabled': recipeName === '' || selectedIngredients.length === 0 || steps.length === 0  || recipeImage === ''}"
-      class="send-button"
-      type="submit"
+  <div class="recipe-form-content">
+    <form  @submit.prevent="createRecipe(this.recipeName, this.steps, this.selectedIngredients, this.recipeImage)">
       
-    >
-      Enviar receta
-    </button>
-    
-    
-  </form>
+      <div class="form-column">
+        <div class="form-block form-recipe-name">
+          <label class="ingredients-title" for="recipe-name">Nombre de la receta</label>
+          <input type="text" v-model="recipeName" placeholder="Ej: Salteado de champiñones" id="name" name="name" required minlength="4" maxlength="8" size="10" />
+        </div>
+      </div>
+      
+
+      <div class="form-column">
+        <div class="form-block form-recipe-name">
+          <label class="ingredients-title" for="recipe-name">Ingredientes básicos</label>
+          <div class="cards-container">
+            <ingredientCard
+              v-for="ingredient in ingredients"
+              :selectedIngredients="selectedIngredients"
+              :ingredients="ingredients"
+              :ingredient="ingredient"
+              :key="ingredient.id"
+            />
+            <!-- <div class="ingredient-card"  > 
+                    </div> -->
+          </div>
+        </div>
+      </div>
+      
+
+      <div class="form-column">
+        <div class="form-block recipe-steps-container form-recipe-name">
+          <label for="steps">Pasos</label>
+          <ol>
+            <newStep
+              v-for="step in steps"
+              :key="step"
+              :steps="steps"
+              @add-step="(descriptionStep) => addStep(descriptionStep)"
+            />
+          </ol>
+        </div>
+
+        <div class="form-block form-recipe-name">
+          <label class="ingredients-title" for="recipe-img">Imagen de la receta</label>
+          <input id="recipe-img" v-model="recipeImage" />
+        </div>
+
+        <button :class="{ 'disabled': recipeName === '' || selectedIngredients.length === 0 || steps.length === 0  || recipeImage === ''}"
+          class="send-button"
+          type="submit"
+          
+        >
+          Enviar receta
+        </button>
+      </div>
+      
+
+      
+      
+      
+      
+    </form>
+  </div>
 
   <div class="sentMessage" v-if="isSent">
     <p>Tu receta se ha enviado con éxito a nuestro repositorio</p>
@@ -114,14 +130,24 @@ export default {
 </template>
 
 <style>
-    form .form-recipe-name input {
-        background-color: #FAFAFA!important;
+
+    form .form-block.form-recipe-name {
+      height: initial;
+    }
+  
+    form .form-block.form-recipe-name input {
+        background-color: #FAFAFA;
         border-radius: 10px;
         border: 1px solid #EFEFEF;
     }
     form button.send-button.disabled {
       background-color: grey;
     }
+
+    form .form-block.form-recipe-name input.required {
+      border: 1px solid var(--receke-red);
+    }
+
     .sentMessage {
       display: flex;
       text-align: center;
@@ -136,9 +162,27 @@ export default {
       height: 100vh;
       background-color: #EDEDD4;
     }
+
     .sentMessage p {
       color: black;
       margin-bottom: 20px;
+    }
+
+    @media (min-width: 1024px) {
+      form {
+        width: 100%;
+        display: grid;
+        gap: 20px;
+        grid-template-columns: 1fr 1fr 1fr;
+      }
+
+      form .form-column .form-block {
+        width: 100%;
+      }
+
+      form .form-column .form-block .cards-container {
+        height: 50vh;
+      }
     }
 
    
